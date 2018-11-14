8b7b299fd0e1c787336b65802f0857c1bb5f022c
Johnny Melo <Johnnymelowork@gmail.com>
2018-11-14 10:01:10 -0500

    working on object

---
+++ assets/JavaScript/game.js
@@ -0,0 +1,29 @@
+
+// Creating crystal variable
+var thunderStone = 0;
+var waterStone = 0;
+var leafStone = 0;
+var fireStone = 0;
+
+// Set up as objects 
+const obj = {}
+
+jQuery(itemsFromDom).each(function() {
+  const element = jQuery(this)
+  const name = element.attr('id')
+  const value = element.attr('value')
+
+// Make the variables and scores for wins and losses
+
+// Go into making the function
+
+// Randomize the funcion number
+
+// Start game function
+
+// Set all variables to the inital values (to the game)
+
+// Put each object to a number (look back at comment #1)
+
+
+
