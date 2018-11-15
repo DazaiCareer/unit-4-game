fbbe3ec147d6c94b88eb71ea623bae95f9a10beb
Johnny Melo <Johnnymelowork@gmail.com>
2018-11-14 22:14:21 -0500

    inital commit

--- assets/JavaScript/game.js
+++ assets/JavaScript/game.js
@@ -1,29 +1,86 @@
 
-// Creating crystal variable
-var thunderStone = 0;
-var waterStone = 0;
-var leafStone = 0;
-var fireStone = 0;
+var crystalValue1;
+var crystalValue2;
+var crystalValue3;
+var crystalValue4;
+var targetNumber;
+var totalSum;
+var wins = 0;
+var Losses = 0;
 
-// Set up as objects 
-const obj = {}
+function start() {
+    targetNumber= Math.floor(Math.random() * 101) + 19;
+    $("#randomNumber").text(targetNumber);
+    crystalValue1= Math.floor(Math.random() * 11) + 1;
+    crystalValue2= Math.floor(Math.random() * 11) + 1;
+    crystalValue3= Math.floor(Math.random() * 11) + 1;
+    crystalValue4= Math.floor(Math.random() * 11) + 1;
+    totalSum = 0;
+    $("#yourSum").text(totalSum);
+}
 
-jQuery(itemsFromDom).each(function() {
-  const element = jQuery(this)
-  const name = element.attr('id')
-  const value = element.attr('value')
+start();
 
-// Make the variables and scores for wins and losses
-
-// Go into making the function
-
-// Randomize the funcion number
-
-// Start game function
-
-// Set all variables to the inital values (to the game)
-
-// Put each object to a number (look back at comment #1)
-
+$("#crystal1").on("click", function() {
+totalSum = totalSum + crystalValue1;
+console.log(totalSum);
+$("#yourSum").text(totalSum);
+if (totalSum === targetNumber) {
+    wins++;
+    $("#wins").text(wins);
+    alert("you won!");
+    start();
+} else if (totalSum > targetNumber) {
+    losses = losses+1;
+    $("#losses").text(losses);
+    alert("sorry you lost!");
+    start();
+}
+})
+$("#crystal2").on("click", function() {
+totalSum = totalSum + crystalValue2;
+$("#yourSum").text(totalSum);
+if (totalSum === targetNumber) {
+    wins++;
+    $("#wins").text(wins);
+    alert("you won!");
+    start();
+} else if (totalSum > targetNumber) {
+    losses++;
+    $("#losses").text(losses);
+    alert("sorry you lost!");
+    start();
+}
+})
+$("#crystal3").on("click", function() {
+totalSum = totalSum + crystalValue3;
+$("#yourSum").text(totalSum);
+if (totalSum === targetNumber) {
+    wins++;
+    $("#wins").text(wins);
+    alert("you won!");
+    start();
+} else if (totalSum > targetNumber) {
+    losses++;
+    $("#losses").text(losses);
+    alert("sorry you lost!");
+    start();
+}
+})
+$("#crystal4").on("click", function() {
+totalSum = totalSum + crystalValue4;
+$("#yourSum").text(totalSum);
+if (totalSum === targetNumber) {
+    wins++;
+    $("#wins").text(wins);
+    alert("you won!");
+    start();
+} else if (totalSum > targetNumber) {
+    losses++;
+    $("#losses").text(losses);
+    alert("sorry you lost!");
+    start();
+}
+})
 
 
